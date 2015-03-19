0c50539ff78a764e3804b2693a6082fc37501f6e
Alexander Suter <alexander.suter@gmx.ch>
2015-03-19 13:25:07 +0100

    default properties definied no runtime properties checkedin FotoShootProperties works now with default Properties

--- src/main/java/accefa/properties/FotoShootProperties.java
+++ src/main/java/accefa/properties/FotoShootProperties.java
@@ -12,39 +12,64 @@ public class FotoShootProperties {
 
    private static final String PROPERTY_URL = "URL";
 
-   private static final String FILENAME = "runtime/fotoshoot-ui.properties";
+   private static final String FILE_RUNTIME = "runtime/fotoshoot-ui.properties";
+
+   private static final String FILE_DEFAULT = "default.fotoshoot-ui.properties";
 
    private Properties clientProperties;
 
+   /**
+    * Lädt die Werte aus den Dateien. Falls keine Runtime-Properties existieren,
+    * werden die Default Properties geladen.
+    */
    public void load() {
       clientProperties = new Properties();
       try {
-         final InputStream stream = new FileInputStream(FILENAME);
+         InputStream stream = null;
+         if (new File(FILE_RUNTIME).exists()) {
+            stream = new FileInputStream(FILE_RUNTIME);
+         } else {
+            stream = this.getClass().getClassLoader().getResourceAsStream(FILE_DEFAULT);
+         }
          if (stream != null) {
             clientProperties.load(stream);
          }
       } catch (final IOException io) {
-         System.err.println("Could not load " + FILENAME + " use default configuration.");
+         System.err.println("Could not load " + FILE_RUNTIME + " use default configuration.");
       }
    }
 
+   /**
+    * Persitiert die Werte.
+    */
    public void save() {
       try {
-         final File file = new File(FILENAME);
+         final File file = new File(FILE_RUNTIME);
          if (!file.getParentFile().exists()) {
             file.getParentFile().mkdir();
          }
-         final OutputStream out = new FileOutputStream(FILENAME);
+         final OutputStream out = new FileOutputStream(FILE_RUNTIME);
          clientProperties.store(out, null);
       } catch (final IOException e) {
          e.printStackTrace();
       }
    }
 
+   /**
+    * Gibt den Wert der URL zurück.
+    *
+    * @return URL.
+    */
    public String getUrl() {
       return clientProperties.getProperty(PROPERTY_URL);
    }
 
+   /**
+    * Setzt die URL.
+    *
+    * @param url
+    *           Url.
+    */
    public void setUrl(final String url) {
       clientProperties.setProperty(PROPERTY_URL, url);
    }
