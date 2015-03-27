b104f816712645afcce3fc758a2605980e3a7b14
Fabian Wüthrich <fabian.wuethrich@outlook.com>
2015-03-27 09:38:38 +0100

    ImageConfigModel angepasst

--- src/test/java/accefa/ui/models/ImageConfigModelTest.java
+++ src/test/java/accefa/ui/models/ImageConfigModelTest.java
@@ -1,7 +1,8 @@
 package accefa.ui.models;
 
-import static org.junit.Assert.*;
-import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
 import javafx.beans.property.BooleanProperty;
 import javafx.beans.property.IntegerProperty;
 
@@ -11,86 +12,88 @@ import org.junit.Test;
 public class ImageConfigModelTest {
 
 	private ImageConfigModel model;
-	
+
 	@Before
 	public void setUp() {
 		model = new ImageConfigModel();
 	}
-	
+
 	@Test
-	public void setGetLineY() {
-	    model.setLineY(DetectionDefault.LINE_Y);
-	    assertEquals(DetectionDefault.LINE_Y, model.getLineY());
+	public void testSetGetLineY() {
+		model.setLineY(DetectionDefault.LINE_Y);
+		assertEquals(DetectionDefault.LINE_Y, model.getLineY());
 	}
-	
+
 	@Test
-	public void lineYProperty() {
+	public void testLineYProperty() {
 		assertThat(model.lineYProperty(), instanceOf(IntegerProperty.class));
 	}
-	
+
 	@Test
-	public void setGetLineH() {
-	    model.setLineH(DetectionDefault.LINE_H);
-	    assertEquals(DetectionDefault.LINE_H, model.getLineH());
+	public void testSetGetLineH() {
+		model.setLineH(DetectionDefault.LINE_H);
+		assertEquals(DetectionDefault.LINE_H, model.getLineH());
 	}
-	
+
 	@Test
-	public void lineHProperty() {
+	public void testLineHProperty() {
 		assertThat(model.lineHProperty(), instanceOf(IntegerProperty.class));
 	}
-	
+
 	@Test
-	public void setGetGreyscaleThreshold() {
-	    model.setGreyscaleThreshold(DetectionDefault.GREYSCALE_THRESHOLD);
-	    assertEquals(DetectionDefault.GREYSCALE_THRESHOLD, model.getGreyscaleThreshold());
+	public void testSetGetGreyscaleThreshold() {
+		model.setGreyscaleThreshold(DetectionDefault.GREYSCALE_THRESHOLD);
+		assertEquals(DetectionDefault.GREYSCALE_THRESHOLD,
+				model.getGreyscaleThreshold());
 	}
-	
+
 	@Test
-	public void greyscaleThresholdProperty() {
-		assertThat(model.greyscaleThresholdProperty(), instanceOf(IntegerProperty.class));
+	public void testGreyscaleThresholdProperty() {
+		assertThat(model.greyscaleThresholdProperty(),
+				instanceOf(IntegerProperty.class));
 	}
-	
+
 	@Test
-	public void setGetGreyscale() {
-	    model.setGreyscale(DetectionDefault.GREYSCALE);
-	    assertEquals(DetectionDefault.GREYSCALE, model.isGreyscale());
+	public void testSetGetGreyscale() {
+		model.setGreyscale(DetectionDefault.GREYSCALE);
+		assertEquals(DetectionDefault.GREYSCALE, model.isGreyscale());
 	}
-	
+
 	@Test
-	public void greyscaleProperty() {
+	public void testGreyscaleProperty() {
 		assertThat(model.greyscaleProperty(), instanceOf(BooleanProperty.class));
 	}
-	
+
 	@Test
-	public void setGetQuality() {
-	    model.setQuality(DetectionDefault.QUALITY);
-	    assertEquals(DetectionDefault.QUALITY, model.getQuality());
+	public void testSetGetQuality() {
+		model.setQuality(DetectionDefault.QUALITY);
+		assertEquals(DetectionDefault.QUALITY, model.getQuality());
 	}
-	
+
 	@Test
-	public void qualityProperty() {
+	public void testQualityProperty() {
 		assertThat(model.qualityProperty(), instanceOf(IntegerProperty.class));
 	}
-	
+
 	@Test
-	public void setGetCropX() {
-	    model.setCropX(DetectionDefault.CROP_X);
-	    assertEquals(DetectionDefault.CROP_X, model.getCropX());
+	public void testSetGetCropX() {
+		model.setCropX(DetectionDefault.CROP_X);
+		assertEquals(DetectionDefault.CROP_X, model.getCropX());
 	}
-	
+
 	@Test
-	public void cropXProperty() {
+	public void testCropXProperty() {
 		assertThat(model.cropXProperty(), instanceOf(IntegerProperty.class));
 	}
-	
+
 	@Test
-	public void setGetContrast() {
-	    model.setContrast(DetectionDefault.CONTRAST);
-	    assertEquals(DetectionDefault.CONTRAST, model.getContrast());
+	public void testSetGetContrast() {
+		model.setContrast(DetectionDefault.CONTRAST);
+		assertEquals(DetectionDefault.CONTRAST, model.getContrast());
 	}
-	
+
 	@Test
-	public void contrastProperty() {
+	public void testContrastProperty() {
 		assertThat(model.contrastProperty(), instanceOf(IntegerProperty.class));
 	}
 
