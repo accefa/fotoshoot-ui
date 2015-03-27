fd10a0e517ffccee7091168effb3cc1f56652ac8
Fabian Wüthrich <fabian.wuethrich@outlook.com>
2015-03-27 10:01:43 +0100

    DcDrive hinzugefügt

---
+++ src/main/java/accefa/ui/models/DcDriveModel.java
@@ -0,0 +1,56 @@
+package accefa.ui.models;
+
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.SimpleBooleanProperty;
+
+public class DcDriveModel {
+	private BooleanProperty forward = new SimpleBooleanProperty();
+	private BooleanProperty reverse = new SimpleBooleanProperty();
+	private BooleanProperty reset = new SimpleBooleanProperty();
+
+	public boolean isForward() {
+		return forward.get();
+	}
+
+	public void setForward(boolean forward) {
+		this.forward.set(forward);
+	}
+
+	public BooleanProperty forwardProperty() {
+		return forward;
+	}
+
+	public boolean isReverse() {
+		return reverse.get();
+	}
+
+	public void setReverse(boolean reverse) {
+		this.reverse.set(reverse);
+	}
+
+	public BooleanProperty reverseProperty() {
+		return reverse;
+	}
+
+	public boolean isReset() {
+		return reset.get();
+	}
+
+	public void setReset(boolean reset) {
+		this.reset.set(reset);
+	}
+
+	public BooleanProperty resetProperty() {
+		return reset;
+	}
+
+	@Override
+	public String toString() {
+		final StringBuilder stringBuilder = new StringBuilder();
+		stringBuilder.append("Forward: " + isForward() + "\n");
+		stringBuilder.append("Reverse: " + isReverse() + "\n");
+		stringBuilder.append("Reset: " + isReset() + "\n");
+		return stringBuilder.toString();
+	}
+
+}
---
+++ src/test/java/accefa/ui/models/DcDriveDefault.java
@@ -0,0 +1,7 @@
+package accefa.ui.models;
+
+public interface DcDriveDefault {
+	boolean FORWARD_NORMAL = true;
+	boolean REVERT_NORMAL = true;
+	boolean RESET_NORMAL = true;
+}
